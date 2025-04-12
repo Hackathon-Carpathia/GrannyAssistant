014024ba677a687c0168c5f038cbcfdc7c1261fb
pkutyl <01168894@pw.edu.pl>
2025-04-12 13:48:06 +0200

    fix unnecessary async

--- twilio-sms-sender-api/twilio_config.py
+++ twilio-sms-sender-api/twilio_config.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-async def get_twilio_client():
+def get_twilio_client():
     account_sid = os.getenv("TWILIO_ACCOUNT_SID")
     auth_token = os.getenv("TWILIO_AUTH_TOKEN")
     client = Client(account_sid, auth_token)
